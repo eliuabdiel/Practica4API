5b82644b7038a1697127cccc10174dd378192ad8
eliuabdiel <2018030333@upsin.edu.mx>
2021-02-13 03:45:27 -0700

    Add project files.

---
+++ Practica4API/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practica4API.Models
+{
+    public partial class Products
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public float? Precio { get; set; }
+        public string ImgSrc { get; set; }
+    }
+}
---
+++ Practica4API/Models/testContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Practica4API.Models
+{
+    public partial class testContext : DbContext
+    {
+        public testContext()
+        {
+        }
+
+        public testContext(DbContextOptions<testContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=GOsk8DAY$;database=test");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.ToTable("products");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Descripcion)
+                    .HasColumnName("descripcion")
+                    .HasMaxLength(450);
+
+                entity.Property(e => e.ImgSrc)
+                    .HasColumnName("imgSrc")
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Nombre)
+                    .HasColumnName("nombre")
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.Precio).HasColumnName("precio");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
